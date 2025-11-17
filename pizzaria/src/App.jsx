62e3af84f0427ac598b697cbcf886acacd630ad4
Gonzalo.Gonzalez.A <gmx.arostegui@gmail.com>
2025-11-16 22:07:48 -0300

    hito 4 segunda copia

--- pizzaria/src/App.jsx
+++ pizzaria/src/App.jsx
@@ -1,12 +1,12 @@
 import { Card } from "react-bootstrap";
 import Navbar from "./components/Navbar";
-import Home from "./components/Home";
-import CardPizza from "./components/CardPizza";
+// import Home from "./components/Home";
+// import CardPizza from "./components/CardPizza";
 import Footer from "./components/Footer";
-import Login from "./components/Login";
-import RegisterPage from "./components/RegisterPage";
-import Cart from "./components/Cart";
-import React from "react";
+// import Login from "./components/Login";
+// import RegisterPage from "./components/RegisterPage";
+// import Cart from "./components/Cart";
+// import React from "react";
 import Pizzas from "./components/Pizzas";
 
 const App = () => {
